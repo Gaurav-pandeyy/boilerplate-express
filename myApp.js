abf5f36696dc4f4021813d16086efa081db385cf
Gaurav <gauravpande568@gmail.com>
2025-09-18 17:36:43 +0530

    DAY 1

--- myApp.js
+++ myApp.js
@@ -1,39 +1,25 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
+const env = require("dotenv").config()
 
+// Serve static files from "public" folder
+app.use("/public",express.static(path.join(__dirname, "public")));
 
+const absolute_path = path.join(__dirname, "views", "index.html");
 
+app.get("/", function(req, res) {
+    res.sendFile(absolute_path);
+});
 
+app.get("/json",function(req,res){
+    let message = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = message.toUpperCase();
+    }
+    res.json({ "message": message });
+})
 
+console.log("Hello World");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
